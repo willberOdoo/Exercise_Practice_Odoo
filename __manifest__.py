66b8ae8d85f21d0f8fdad513bb62a32ca6fab9d7
“ <willrr.payall.2021@gmail.com>
2022-05-17 10:58:18 -0400

    [ADD] folder and file the structure basic module of Odoo

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Training',
+
+    'summary': """Training technic""",
+
+    'description': """ 
+        exercise practice odoo
+    """,
+    'autor': 'Payall',
+
+    'developer': 'Willber R.R',
+
+    'website': 'htps://www.odoo.com',
+
+    'category': 'Training',
+    'version': '1.0.1',
+
+    'depends': [],
+
+    'data': [],
+
+    'demo': [],
+}
